72fc28121bc61e4115ba3a76886a256b6c4f2065
Ваше Имя <you@example.com>
2020-04-20 00:57:13 +0700

    add frontend

--- state/admin.py
+++ state/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import State
+from .models import State, Status, Block
 
 # Register your models here.
 admin.site.register(State)
+admin.site.register(Status)
+admin.site.register(Block)
